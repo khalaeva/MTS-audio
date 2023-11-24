b83d0dc3ce711a4418f69db9a4f93c39cc7653a5
Kirill Nemtyrev <75456383+kreeeeeel@users.noreply.github.com>
2023-11-25 01:18:04 +0300

    delete trash

--- server-room/src/main/java/com/project/hackathon/hackathon/controller/RoomController.java
+++
@@ -1,7 +0,0 @@
-package com.project.hackathon.hackathon.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/api/room")
-public class RoomController {
-}
