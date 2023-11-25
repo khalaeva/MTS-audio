c077c5910226e4cd21a35e5af387dab07db31254
annoyingnay <125662271+annoyingnay@users.noreply.github.com>
2023-11-25 18:38:45 +0300

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,3 @@
 <template>
     <p>HelloWorld</p>
-<<<<<<< HEAD
 </template>
-
-<script setup>
-=======
-    <CreateRoom/>
-</template>
-
-<script setup>
-import CreateRoom from '../components/CreateRoom.vue'
->>>>>>> 7933ed150d561f723878a9978c5f4707eed6d668
-</script>
